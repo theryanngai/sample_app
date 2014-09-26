fe3203252fbb6c95ed469dafc610bf151b32deff
Ryan Ngai <theryanngai@gmail.com>
2014-09-26 00:59:46 -0700

    Edited Application Helper and Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,7 +18,7 @@ describe "Static pages" do
 
     it "should not have a custom page title" do
       visit '/static_pages/home'
-      expect (page).not_to have_title('| Home')
+      expect(page).not_to have_title('| Home')
     end
   end
 
